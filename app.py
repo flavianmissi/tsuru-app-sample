5959c24ff31263341c52a4cd742a71b9bccadcf7
Flavia Missi <flaviamissi@gmail.com>
2013-08-09 12:03:02 -0300

    api.py: printing unit hostname
    this way we can easily show when the application is running on multiple
    units

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ mysql_db_name = os.environ.get("MYSQL_DATABASE_NAME")
 def index():
     return "Hello world!"
 
+@app.route("/units")
+def units():
+    content += "<h1>host is: {0}</h1>".format(subprocess.check_output("hostname"))
+    return content
+
 
 @app.route("/db")
 def test_connection():
